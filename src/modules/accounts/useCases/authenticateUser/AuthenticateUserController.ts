f045c2f00a268f7e2aa9097070b7310041fbb278
JhonatanMuniz <jhonatanmuniz123@gmail.com>
2021-04-27 16:18:06 -0300

    Update teste app

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserController.ts
@@ -3,7 +3,6 @@ import { container } from "tsyringe";
 
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
-
 class AuthenticateUserController {
     async handle(request: Request, response: Response): Promise<Response> {
         const { password, email } = request.body;
